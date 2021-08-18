1b36aa6dd2ae09f1d2745c8a6a82a4f91dee8ea3
CamDawg <pcamagna@yahoo.com>
2021-08-17 20:55:50 -0700

    Updating packager

--- package_mod.bat
+++ package_mod.bat
@@ -6,7 +6,7 @@ call "%~dp0..\ModPackaging\utilities\ie_games.bat"
 
 REM /* MODIFY: set the values of the 3 variables below to reflect the current mod version */
 set "mod_name=Plasmocat's 'BG NPC Portraits in BG2 Style'"
-set mod_version=v3
+set mod_version=v4
 set mod_folder=plasmobg1bg2
 
 REM /* MODIFY: list here which IE games the mod is compatible with, from this list of possibilities: */
@@ -23,7 +23,7 @@ set ico_folder=%mod_folder%\style
 set audio_folder=%mod_folder%\audio
 set tispack_folder=%mod_folder%\tiz
 set iconv_folder=%mod_folder%\iconv
-set mod_readme=http://htmlpreview.github.io/?https://github.com/Gibberlings3/Documentation/blob/master/readmes/readme-%mod_folder%.html
+set mod_readme=https://gibberlings3.github.io/Documentation/readmes/readme-plasmobg1bg2.html
 
 REM /* MODIFY: if you don't need to build a specific package, you can disable it by setting the appropriate variable below to 0 */
 set build_windows=1
